ce1fefdd846b607105606901390d2ebdfa2bff86
vagelim <vagelim@gmail.com>
2013-01-30 00:46:50 -0500

    Update tweet_backup.py
    added a cleaning function to clean tweet file
    no longer takes a file argument, instead stores tweets in <user>_tweets.html

--- tweet_backup.py
+++ tweet_backup.py
@@ -3,20 +3,53 @@ import sys
 import os
 import commands
 
+def clean_tweets(tweets):
+    tweet_file = open(tweets, 'r')
+    dirty = tweet_file.read()
+    tweet_file.close()
+
+    occur = dirty.count('<title>')
+    clean = "Tweets:" + '<br>'
+    index = dirty.find('<title>') + 7
+    endex = dirty.find('</title>')
+
+    while occur > 0:
+        index = dirty.find('<title>',index) + 7
+        endex = dirty.find('</title>',index)
+        temp = dirty[index:endex]
+
+
+        clean = clean + temp + '<br>'
+        occur = occur - 1
+        
+
+    save = open(tweets,'w')
+    save.write(clean)
+    save.close()
+
+
 def backup():
     user = sys.argv[1]
-    store = sys.argv[2]
+    store = user + '_tweets.html'
     command = "curl -s --user-agent 'Mozilla' --insecure 'https://api.twitter.com/1/statuses/user_timeline.rss?screen_name="
-    append = "&page="
+    count = "&count=100"
+    page = "&page="
     x = 1
     print "This may take a while..."
-    tweet_file = open(store, 'a')
+    tweet_file = open(store, 'w')
     while x < 32:
-        tweets = commands.getoutput(command+user+append+str(x) + "'")
+        tweets = commands.getoutput(command+user+count+page+str(x) + "'")
         tweet_file.write(tweets)
         x = x + 1
         
     tweet_file.close()
 
+    print "Cleaning tweets"
+    clean_tweets(store)
+
 if __name__ == '__main__':
     backup()
+
+
+
+
