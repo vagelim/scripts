1f13463bae7632634f242a66c3b205efc17d0023
vagelim <vagelim@gmail.com>
2013-01-29 19:30:24 -0500

    Update masstxt.py
    added code to take second argument as path to CSV (falling over to hardcoded CSV file if second argument not provided)

--- masstxt.py
+++ masstxt.py
@@ -11,15 +11,23 @@ password = b64.b64decode('ENCODED GV PASS')
 user = b64.b64decode('ENCODED GV USR')
 voice.login(user, password)
 
-phoneNumber = '123-456-7890' #(cell #associated with GV account)
+phoneNumber = '123-456-7890' #(cell number associated with GV account)
 
-textReader = csv.reader(open('/PATH/TO/PARTY/LIST/party_txt_list.txt','rb'), delimiter=',')
-
+#second argument to program is path to a CSV of phone numbers
+try:
+	textReader = csv.reader(open(sys.argv[2],'rb'), delimiter=',')
+except IndexError:
+	#if there is no argument, use hardcoded path
+	textReader = csv.reader(open('/PATH/TO/PARTY/LIST/party_txt_list.txt','rb'), delimiter=',')
+	#change the above path to a file containing phone numbers separated by commas
 text_list = textReader.next()
 
 def massTXT():
+	#mass is a string prepended to the text to indicate it is a group message
+	#leave blank for no prepended text
   mass = "massTXT:"
 	try:
+	#program expects text to be first commandline argument to program
 		text = sys.argv[1]
 	except IndexError:
 		print "Need message"		
