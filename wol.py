c5503b00f86d67e4febea1b4b7d21366075ca522
vagelim <vagelim@gmail.com>
2013-12-13 19:09:17 -0500

    Create wol.py
    sends a magic packet to hard-coded MAC address

---
+++ wol.py
@@ -0,0 +1,6 @@
+##WakeOnLan
+
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.sendto('\xff'*6 + '\x00\x00\x00\x00\x00\x00'*16, ('192.168.1.255', 80)
