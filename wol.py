8ca7ce5922729caac2c70bc7a1bc6935f1cd381c
vagelim <vagelim@gmail.com>
2013-12-15 19:43:58 -0500

    Update wol.py
    fixed error
    (missing closing paren)

--- wol.py
+++ wol.py
@@ -3,4 +3,4 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.sendto('\xff'*6 + '\x00\x00\x00\x00\x00\x00'*16, ('192.168.1.255', 80)
+s.sendto('\xff'*6 + '\x00\x00\x00\x00\x00\x00'*16, ('192.168.1.255', 80))
