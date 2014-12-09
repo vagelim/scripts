09f6b89f0514e3e6322b663ec196aa30392b3736
vagelim <vageli.code@gmail.com>
2014-12-09 15:29:02 -0500

    Create twitter_oauth.py
    Easily generate new authorization tokens using birdy python library

---
+++ twitter_oauth.py
@@ -0,0 +1,17 @@
+#To get twitter oauth tokens
+#1. Get your consumer key and consumer secret from the app page on apps.twitter.com
+#2. Use the birdy python library
+
+from birdy.twitter import UserClient
+
+key = raw_input("Consumer key: ")
+secret = raw_input("Consumer secret: ")
+client = UserClient(key, secret)
+token = client.get_signin_token()
+access_key = token.oauth_token
+access_secret = token.oauth_token_secret
+print token.auth_url
+pin = raw_input("PIN: ")
+client = UserClient(key, secret, access_key, access_secret)
+client.get_access_token(pin)
+print token
