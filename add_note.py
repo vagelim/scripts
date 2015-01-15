6a97305806e4c5fb66df771896110505173c065f
vagelim <vageli.code@gmail.com>
2015-01-15 18:21:59 -0500

    Create add_note.py
    quick little script to add a note to simplenote via the command line

---
+++ add_note.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from simplenote import Simplenote
+
+USER = 'SIMPLENOTE EMAIL ADDRESS HERE'
+PW = 'SIMPLENOTE PASSWORD'
+
+def add_note(note):
+        simple = Simplenote(USER, PW)
+
+        #Get auth
+        token = simple.authenticate(USER, PW)
+	t = simple.add_note(note)
+		
+
+if __name__ == '__main__':
+        import sys
+
+        if len(sys.argv) > 2:
+                note = sys.argv[1:]
+        else:
+                note = sys.argv[1]
+                
+        add_note(note)
