7fa8dfa73024d7a317f6ef391e1cdb0ec4fccfee
vagelim <vageli.code@gmail.com>
2015-01-28 18:03:50 -0500

    Update random_note.py
    added some error catching

--- random_note.py
+++ random_note.py
@@ -9,11 +9,14 @@ def random_note(file):
 	#Choose random number between 0 and the length of pickle
 	number = random.randrange(0, len(t))
 
-	#'content' is the dictionary key with the note content
-	note = t[number]['content']
-	print note
+#'content' is the dictionary key with the note content
+	try:
+		note = t[number]['content']
+		print note
+		return note
+	except TypeError:
+		random_note(file)
 
-	return note
 
 if __name__ == '__main__':
 	import sys
