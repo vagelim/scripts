46c1d2367fa78b24b7ddb64b5644ab7f42c29399
vagelim <vageli.code@gmail.com>
2015-05-01 18:27:29 -0400

    Update wordNeighbors.py

--- wordNeighbors.py
+++ wordNeighbors.py
@@ -1,7 +1,7 @@
 def search(text, string, n):
     '''Searches for text, and retrieves n words either side of the text, which are returned separately'''
     word = r"\W*([\w]+)"
-    groups = re.search(r'{}\W*{}{}'.format(word*n,string ,word*n),t).groups()
+    groups = re.search(r'{}\W*{}{}'.format(word*n,string ,word*n),text).groups()
     left = list(groups[:n])
     right = list(groups[n:])
     #return groups[:n],groups[n:]
