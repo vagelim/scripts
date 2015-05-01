446a274910dcf1a544cfdabcf15ab59de3ad93ae
vagelim <vageli.code@gmail.com>
2015-05-01 17:35:49 -0400

    Create wordNeighbors.py
    find neighboring words where n is the number of neighbors to return on each side of the word

---
+++ wordNeighbors.py
@@ -0,0 +1,8 @@
+def search(text, string, n):
+    '''Searches for text, and retrieves n words either side of the text, which are returned separately'''
+    word = r"\W*([\w]+)"
+    groups = re.search(r'{}\W*{}{}'.format(word*n,string ,word*n),t).groups()
+    left = list(groups[:n])
+    right = list(groups[n:])
+    #return groups[:n],groups[n:]
+    return left,right
