b95610290250eb185a18a1782fab613821b36e74
Evan Mouzakitis <evan@datadoghq.com>
2015-11-18 10:30:05 -0500

    Update wordNeighbors.py
    added find_nth (find nth occurrence of substring in a string)

--- wordNeighbors.py
+++ wordNeighbors.py
@@ -1,4 +1,4 @@
-def search(text, string, n):
+def wordNeighbors(text, string, n):
     '''Searches for text, and retrieves n words either side of the text, which are returned separately'''
     word = r"\W*([\w]+)"
     groups = re.search(r'{}\W*{}{}'.format(word*n,string ,word*n),text).groups()
@@ -6,3 +6,11 @@ def search(text, string, n):
     right = list(groups[n:])
     #return groups[:n],groups[n:]
     return left,right
+
+def find_nth(haystack, needle, n):
+"""Take a string, haystack, and find the nth occurrence of the substring needle"""
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
